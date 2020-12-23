b51cbab9d530f291e3a9d95548e2d90ab027eced
daletom <tom@imgix.com>
2020-12-23 09:06:50 -0800

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <blur-hash-image
+      width="400"
+      height="300"
+      hash="LdHfL}oJR$WBKnfi%3ofT0kCM{ay"
+      src="https://images.unsplash.com/photo-1545910684-8e7c081be9b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80"
+      alt="green lawn grass during daytime"
+    />
+  </div>
+</template>
+
+<script>
+import VueBlurHash from "vue-blurhash";
+
+export default {
+  name: "Home",
+  components: {
+    VueBlurHash
+  }
+};
+</script>
