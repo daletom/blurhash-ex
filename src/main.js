4fd4ba00f7de8558e1f71b8b27eaa85960862b6d
daletom <tom@imgix.com>
2020-12-27 11:28:53 -0800

    update blurhash

--- src/main.js
+++ src/main.js
@@ -3,13 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import VueBlurHash from "vue-blurhash";
+import 'vue-blurhash/dist/vue-blurhash.css'
 
 Vue.config.productionTip = false;
-
+Vue.use(VueBlurHash);
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount("#app");
-
-Vue.use(VueBlurHash);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="home">
+  <div>
     <blur-hash-image
       width="400"
       height="300"
       hash="LdHfL}oJR$WBKnfi%3ofT0kCM{ay"
-      src="https://images.unsplash.com/photo-1545910684-8e7c081be9b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80"
+      src="https://images.unsplash.com/photo-1545910684-8e7c081be9b0?auto=format&fit=crop&w=400&h=300&q=80"
       alt="green lawn grass during daytime"
+      class="gallery-image"
+    />
+    <blur-hash-image
+      width="400"
+      height="300"
+      hash="https://images.unsplash.com/photo-1601758260892-a62c486ace97?fm=blurhash&w=400&h=300&fit=crop"
+      src="https://images.unsplash.com/photo-1601758260892-a62c486ace97?auto=format&fit=crop&w=400&h=300&q=80"
+      alt="green lawn grass during daytime"
+      class="gallery-image"
     />
   </div>
 </template>
@@ -15,3 +24,9 @@ export default {
   name: "Home"
 };
 </script>
+<style scoped>
+.gallery-image {
+  width: 400px;
+  height: 300px
+}
+</style>
