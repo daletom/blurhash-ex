9e1d92dd2156c509404ab6ffefc7fac8f016caeb
daletom <tom@imgix.com>
2020-12-28 16:22:49 -0800

    fix to push to bhash

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,11 @@
 <template>
   <section>
-    <div v-for="image in images" :key="image">
-      <h2>{{ blurred }}</h2>
+    <div v-for="(image, idx) in images" :key="idx">
       <blur-hash-image
         width="400"
         height="300"
-        hash="LdHfL}oJR$WBKnfi%3ofT0kCM{ay"
-        :src="image"
+        :hash="image.bhash"
+        :src="image.url"
         alt="green lawn grass during daytime"
         class="gallery-image"
       />
@@ -18,27 +17,52 @@
 export default {
   name: "Home",
   data() {
-    return {
-      images: [
-        "https://images.unsplash.com/photo-1545910684-8e7c081be9b0?auto=format&fit=crop&w=400&h=300&q=80",
-        "https://images.unsplash.com/photo-1601758260892-a62c486ace97?auto=format&fit=crop&w=400&h=300&q=80"
-      ],
-      blurred: []
+  return {
+    images: [
+      {
+        url: 'https://images.unsplash.com/photo-1545910684-8e7c081be9b0?fit=crop&w=400&h=300&q=80',
+        bhash: ''
+      },
+      {
+        url: 'https://images.unsplash.com/photo-1601758260892-a62c486ace97?fit=crop&w=400&h=300&q=80',
+        bhash: ''
+      }
+    ],
+  }
+},
+asyncComputed: {
+  async getBlurhash() {
+    let requestOptions = {
+      method: "GET",
+      redirect: "follow",
     };
+    const blurhashifications = this.images.map((img, i) => {
+      return fetch(img.url + '&fm=blurhash', requestOptions)
+        .then((res) => res.text())
+        .then((bhash) => {
+          console.log('Eloi, Eloi, lama sabachthani', bhash)
+          this.images[i].bhash = bhash
+        })
+    })
+    return await Promise.all(blurhashifications).catch((error) => console.log("error", error));
   },
-  computed: {
+},
+  /*computed: {
     getBlurhash() {
       return this.image + "&fm=blurhash";
     }
   },
   created: {
-    storeBlur() {
-      fetch(this.image + "&fm=blurhash")
-      .then(response => response.json())
-      .then(data => (this.blurred = data.total))
-      return this.blurred;
+    getBlurry() {
+      return this.image + "&fm=blurhash";
     }
-  }
+    storeBlur() {
+      return fetch(this.image + "&fm=blurhash")
+      .then(response => response.data)
+      //.then(data => (this.blurred = data.total))
+      //return this.blurred;
+    }
+  }*/
 };
 </script>
 <style scoped>
